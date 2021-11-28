a30612c6f7071890a46a67142dfb5836372287b4
Adam Catalfano <acatalfano@gmail.com>
2021-11-27 23:19:27 -0500

    automate provisioning of producers and cloud-provisioner

--- file_srcs/producer.py
+++ file_srcs/producer.py
@@ -7,47 +7,81 @@ from kafka import KafkaProducer
 import logging
 import json
 import jsbeautifier
+import random
 
 
-# topic to produce:
-topic = 'TOPIC'
+def produce() -> None:
+    topic = 'TOPIC'
 
-# uncomment to enable more verbose kafka logging
-# logging.basicConfig(level=logging.DEBUG)
+    # uncomment to enable more verbose kafka logging
+    logging.basicConfig(level=logging.DEBUG)
 
-bootstrap_server = '50.19.186.89'
-kafka_port = '9092'
+    bootstrap_server = '50.19.186.89'
+    kafka_port = '9092'
 
-# acquire the producer
-producer = KafkaProducer(bootstrap_servers=[f'{bootstrap_server}:{kafka_port}'])
+    # acquire the producer
+    producer = KafkaProducer(
+        bootstrap_servers=[f'{bootstrap_server}:{kafka_port}'])
 
-app_id = 'APP_ID'
-api_key = 'API_KEY'
-base_url = 'api_url'
-endpoint = 'api_endpoint'
-url_options = 'url_options'
+    loop_based: bool = False
 
-request_url = f'{base_url}{endpoint}{url_options}'
-response = requests.get(request_url)
-
-if response:
-    content = response.json()
-
-    # get list of news stories
-    data: list = content['data']
+    data = batch_call_data() if loop_based else multi_call_data()
 
     beautifier_opts = jsbeautifier.default_options()
     beautifier_opts.indent_size = 2
 
     for datum in data:
-        # timestamp event was sent
-        timestamp = datetime.timestamp(datetime.utcnow())
-        datum['timestamp'] = timestamp
         datum_content = json.dumps(datum)
         print(jsbeautifier.beautify(datum_content, beautifier_opts))
         producer.send(topic, value=bytes(datum_content, 'ascii'))
         producer.flush()
         time.sleep(1)
 
-# we are done
-producer.close()
+    # we are done
+    producer.close()
+
+
+def multi_call_data() -> list:
+    # global app_id, api_key, base_url, endpoint, request_url, url_options
+    loop_count = 0
+    data = []
+    for _ in range(0, loop_count):
+        # next comment is a synchronization point, to be leveraged by ansible
+        # insert loop logic here
+        endpoint_suffix = 'endpoint_suffix'
+        response = requests.get(build_url(endpoint_suffix))  # (request_url)
+        if response:
+            timestamp = datetime.timestamp(datetime.utcnow())
+            response['timestamp'] = timestamp
+            data.append(response)
+
+    return data
+
+
+def batch_call_data() -> list:
+    # global app_id, api_key, base_url, data_index, endpoint, request_url, url_options
+    endpoint_suffix = 'endpoint_suffix'
+    response = requests.get(build_url(endpoint_suffix))  # (request_url)
+
+    data = response.json()[data_index] if response else []
+
+    for datum in data:
+        # timestamp event was sent
+        timestamp = datetime.timestamp(datetime.utcnow())
+        datum['timestamp'] = timestamp
+
+    return data
+
+
+def build_url(endpoint_suffix: str) -> str:
+    app_id = 'APP_ID'
+    api_key = 'API_KEY'
+    base_url = 'api_url'
+    data_index = 'DATA_INDEX'
+    endpoint = 'api_endpoint'
+    url_options = 'url_options'
+    return f'{base_url}{endpoint}{endpoint_suffix}?{url_options}'
+
+
+if __name__ == '__main__':
+    produce()
